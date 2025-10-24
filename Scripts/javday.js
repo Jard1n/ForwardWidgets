485090e31aec9d3c73d4ac3c47639c04ba8b71c7
Jard1n <151329813+Jard1n@users.noreply.github.com>
2025-10-24 10:26:18 +0800

    Update comment syntax in javday.js

--- Scripts/javday.js
+++ Scripts/javday.js
@@ -1,4 +1,4 @@
-// 引用地址：https://gist.githubusercontent.com/bemarkt/17e04f66b772d4ab9ab839c12a8ad608/raw/javday.js
+// 引用：https://gist.githubusercontent.com/bemarkt/17e04f66b772d4ab9ab839c12a8ad608/raw/javday.js
 var WidgetMetadata = {
   id: "ti.bemarkt.javday",
   title: "JAVDay",
